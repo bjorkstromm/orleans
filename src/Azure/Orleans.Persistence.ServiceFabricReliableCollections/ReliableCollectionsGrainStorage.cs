bc3d59bcf246f7ddc9bbfc74ab1fc81964ec4537
Martin Björkström <martin.bjorkstrom@gmail.com>
2018-10-19 18:53:53 +0300

    Add IReliableStatemanager to Grain storage.

--- src/Azure/Orleans.Persistence.ServiceFabricReliableCollections/ReliableCollectionsGrainStorage.cs
+++ src/Azure/Orleans.Persistence.ServiceFabricReliableCollections/ReliableCollectionsGrainStorage.cs
@@ -4,6 +4,8 @@ using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.ServiceFabric.Data;
+using Microsoft.ServiceFabric.Data.Collections;
 using Orleans.Configuration;
 using Orleans.Configuration.Overrides;
 using Orleans.Runtime;
@@ -17,11 +19,15 @@ namespace Orleans.Hosting
         private readonly ReliableCollectionsStorageOptions options;
         private readonly ClusterOptions clusterOptions;
         private readonly string name;
+        private readonly IReliableStateManager stateManager;
         private readonly SerializationManager serializationManager;
         private readonly IGrainFactory grainFactory;
         private readonly ITypeResolver typeResolver;
         private readonly ILoggerFactory loggerFactory;
         private readonly ILogger logger;
+
+        private IReliableDictionary<string, byte[]> stateDictionary;
+        private readonly AsyncLock asyncLock = new AsyncLock();
 
         /// <summary> Default constructor </summary>
         public ReliableCollectionsGrainStorage(string name,
@@ -33,6 +39,7 @@ namespace Orleans.Hosting
             ITypeResolver typeResolver,
             ILoggerFactory loggerFactory)
         {
+            this.stateManager = stateManager;
             this.options = options;
             this.clusterOptions = clusterOptions.Value;
             this.name = name;
@@ -44,19 +51,42 @@ namespace Orleans.Hosting
             this.logger = this.loggerFactory.CreateLogger(loggerName);
         }
 
-        public Task ClearStateAsync(string grainType, GrainReference grainReference, IGrainState grainState)
+        public async Task ClearStateAsync(string grainType, GrainReference grainReference, IGrainState grainState)
         {
-            throw new System.NotImplementedException();
+            var keyString = grainReference.GrainId.ToParsableString();
+            using (var tx = this.stateManager.CreateTransaction())
+            {
+                var storage = await this.GetStorage();
+                await storage.TryRemoveAsync(tx, keyString);
+                await tx.CommitAsync();
+            }
         }
 
-        public Task ReadStateAsync(string grainType, GrainReference grainReference, IGrainState grainState)
+        public async Task ReadStateAsync(string grainType, GrainReference grainReference, IGrainState grainState)
         {
-            throw new System.NotImplementedException();
+            var keyString = grainReference.GrainId.ToParsableString();
+            using (var tx = this.stateManager.CreateTransaction())
+            {
+                var storage = await this.GetStorage();
+                var result = await storage.TryGetValueAsync(tx, keyString);
+                if (result.HasValue)
+                {
+                    grainState.State = this.serializationManager.DeserializeFromByteArray<object>(result.Value);
+                }
+                await tx.CommitAsync();
+            }
         }
 
-        public Task WriteStateAsync(string grainType, GrainReference grainReference, IGrainState grainState)
+        public async Task WriteStateAsync(string grainType, GrainReference grainReference, IGrainState grainState)
         {
-            throw new System.NotImplementedException();
+            var keyString = grainReference.GrainId.ToParsableString();
+            using (var tx = this.stateManager.CreateTransaction())
+            {
+                var storage = await this.GetStorage();
+                var bytes = this.serializationManager.SerializeToByteArray(grainState.State);
+                await storage.SetAsync(tx, keyString, bytes);
+                await tx.CommitAsync();
+            }
         }
 
         public void Participate(ISiloLifecycle lifecycle)
@@ -72,6 +102,25 @@ namespace Orleans.Hosting
         private Task Close(CancellationToken cancellationToken)
         {
             return Task.CompletedTask;
+        }
+
+        private ValueTask<IReliableDictionary<string, byte[]>> GetStorage()
+        {
+            if (this.stateDictionary != null) return new ValueTask<IReliableDictionary<string, byte[]>>(this.stateDictionary);
+
+            return Async();
+
+            async ValueTask<IReliableDictionary<string, byte[]>> Async()
+            {
+                using (await asyncLock.LockAsync())
+                {
+                    if (this.stateDictionary == null)
+                    {
+                        this.stateDictionary = await this.stateManager.GetOrAddAsync<IReliableDictionary<string, byte[]>>(this.options.StateName ?? this.name);
+                    }
+                    return this.stateDictionary;
+                }
+            }
         }
     }
 
--- src/Orleans.Core.Abstractions/Properties/AssemblyInfo.cs
+++ src/Orleans.Core.Abstractions/Properties/AssemblyInfo.cs
@@ -5,6 +5,7 @@
 [assembly: InternalsVisibleTo("Orleans.TestingHost")]
 [assembly: InternalsVisibleTo("Orleans.TestingHost.AppDomain")]
 [assembly: InternalsVisibleTo("OrleansManager")]
+[assembly: InternalsVisibleTo("Orleans.Persistence.ServiceFabricReliableCollections")]
 
 [assembly: InternalsVisibleTo("DefaultCluster.Tests")]
 [assembly: InternalsVisibleTo("NonSilo.Tests")]
